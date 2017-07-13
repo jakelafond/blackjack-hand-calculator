d550bee039c1e2f86e68023f13c3f6b9f7621ef2
Jake LaFond <jakelafond@gmail.com>
2017-07-13 14:46:57 -0400

    Done

--- scripts/main.js
+++ scripts/main.js
@@ -5,17 +5,33 @@
    to watch the assertions pass as you write your code.
 
    Also remember, that the parameter `hand` will be an array, so
-   you'll need to parse through that first before you can start to 
+   you'll need to parse through that first before you can start to
    write your logic.
 */
 
-
-function handValue (hand) {
-
-
-  return;
+function handValue(hand) {
+  var handArray = [];
+  var handTotal = 0;
+  hand.forEach(hand => {
+    if (isNaN(hand) == false) {
+      handArray.push(hand);
+    } else if (hand === 'A') {
+      handArray.push('11');
+    } else if (hand === 'K' || 'Q' || 'J') {
+      handArray.push('10');
+    }
+  });
+  for (var i = 0; i < handArray.length; i++) {
+    handTotal += Number(handArray[i]);
+  }
+  do {
+    if (handTotal > 21 && hand.includes('A')) {
+      handTotal = handTotal - 10;
+    }
+  } while (handTotal > 21);
+  console.log(handTotal);
+  return handTotal;
 }
-
 
 /* -----  Hints ------
 
